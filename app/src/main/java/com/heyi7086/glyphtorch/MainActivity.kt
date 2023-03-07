a46ea2aa335349220ba3365ecde3f5c165cbe0cb
HE7086 <me@heyi7086.com>
2023-03-07 15:04:20 +0100

    add menu ui

--- app/src/main/java/com/heyi7086/glyphtorch/MainActivity.kt
+++ app/src/main/java/com/heyi7086/glyphtorch/MainActivity.kt
@@ -5,7 +5,10 @@ import android.util.Log
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -13,19 +16,27 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
 import androidx.compose.material.icons.filled.Clear
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Slider
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.heyi7086.glyphtorch.ui.theme.GlyphTorchTheme
@@ -46,15 +57,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GlyphTorchTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background,
-                ) {
-                    Column {
-                        BrightnessSlider(Modifier.padding(16.dp))
-                        ActivateButton(Modifier.fillMaxSize().padding(40.dp))
-                    }
-                }
+                MainContent()
             }
         }
 
@@ -62,7 +65,8 @@ class MainActivity : ComponentActivity() {
         val model = android.os.Build.MODEL
         // are there any other models?
         if (brand != "Nothing" || model != "A063") {
-            Toast.makeText(this,
+            Toast.makeText(
+                this,
                 "It seems that you are not using Nothing Phone (1)",
                 Toast.LENGTH_LONG
             ).show()
@@ -74,6 +78,65 @@ class MainActivity : ComponentActivity() {
         led.setBrightness(0)
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
+    @Composable
+    fun MainContent() {
+        var showMenu by remember {
+            mutableStateOf(false)
+        }
+
+        Scaffold(
+            topBar = {
+                CenterAlignedTopAppBar(
+                    title = {
+                        Text(
+                            getString(R.string.app_name),
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                    },
+                    actions = {
+                        IconButton(onClick = { showMenu = !showMenu }) {
+                            Icon(
+                                imageVector = Icons.Filled.MoreVert,
+                                contentDescription = "Localized description"
+                            )
+                        }
+                        DropdownMenu(
+                            expanded = showMenu,
+                            onDismissRequest = { showMenu = false }
+                        ) {
+                            DropdownMenuItem(
+                                text = {
+                                    Row {
+                                        Icon(
+                                            imageVector = Icons.Default.Check,
+                                            contentDescription = "",
+                                        )
+                                        Spacer(Modifier.size(8.dp))
+                                        Text("Test")
+                                    }
+                                },
+                                onClick = { /*TODO*/ }
+                            )
+                        }
+                    }
+                )
+            },
+            content = {
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(it),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    BrightnessSlider(Modifier.padding(16.dp))
+                    ActivateButton()
+                }
+            }
+        )
+    }
+    
     @Composable
     fun BrightnessSlider(modifier: Modifier = Modifier) {
         var sliderValue by remember {
@@ -122,7 +185,8 @@ class MainActivity : ComponentActivity() {
                         Icons.Default.Check
                     } else {
                         Icons.Default.Clear
-                    }),
+                    }
+                ),
                 contentDescription = "Torch Status",
                 Modifier.size(40.dp)
             )
