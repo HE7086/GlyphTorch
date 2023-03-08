1e7ef5f618f969446a965b153a006484dc22580d
HE7086 <me@heyi7086.com>
2023-03-08 15:06:21 +0100

    add paths to other single leds

--- app/src/main/java/com/heyi7086/glyphtorch/LedManager.kt
+++ app/src/main/java/com/heyi7086/glyphtorch/LedManager.kt
@@ -2,21 +2,37 @@ package com.heyi7086.glyphtorch
 
 import com.topjohnwu.superuser.Shell
 
-const val BRIGHT1 = "/sys/devices/platform/soc/984000.i2c/i2c-0/0-0020/leds/aw210xx_led/all_white_leds_br"
-const val BRIGHT2 = "/sys/devices/platform/soc/984000.i2c/i2c-0/0-0020/leds/aw210xx_led/glo_current"
+const val BASE = "/sys/devices/platform/soc/984000.i2c/i2c-0/0-0020/leds/aw210xx_led"
+const val BRIGHT1 = "all_white_leds_br"
+const val BRIGHT2 = "glo_current"
+
+// top left led around the camera
+const val REAR_CAM = "rear_cam_led_br"
+
+// top right led
+const val FRONT_CAM = "front_cam_led_br"
+
+// round led around the coil
+const val ROUND = "round_leds_br"
+
+// bottom center led line
+const val VLINE = "vline_leds_br"
+
+// bottom center led dot
+const val DOT = "dot_led_br"
 
 object LedManager {
     init {
         Shell.getShell()
-        Shell.cmd("echo 4095 > $BRIGHT1").exec()
+        Shell.cmd("echo 4095 > $BASE/$BRIGHT1").exec()
     }
 
     fun setBrightness(value: Int) {
-        Shell.cmd("echo $value > $BRIGHT2").exec()
+        Shell.cmd("echo $value > $BASE/$BRIGHT2").exec()
     }
 
     fun getBrightness(): Int {
-        val result = Shell.cmd("cat $BRIGHT2").exec()
+        val result = Shell.cmd("cat $BASE/$BRIGHT2").exec()
         return result.out.last().toInt()
     }
 
